8ae12ae7e5efff13a22c030bfee09c46a6c5b85f
SimonVestman <simonw202@gmail.com>
2019-09-13 11:13:16 +0200

    Variabler

--- uppgift2_1.java
+++ uppgift2_1.java
@@ -1,5 +1,26 @@
+/*
+Uppgift 2.1 i boken sida 34
+Skapad 2019 09 13
+Av simon Vestman
+*/
+
+import	javax.swing.*;
+
 public class uppgift2_1{
-	public static void main(string[] args) {
+	public static void main(String[] args) {
+		String namn;
+		String adress;
+		String tfnr;
+		String allt;
 		
+		namn = "Simon Vestman";
+		adress = "Vastamarken78";
+		tfnr = "0730735029";
+		allt = namn + 
+				"\n"+ adress +
+					"\n" + tfnr;
+		
+		
+		JOptionPane.showMessageDialog(null,allt);
 	}
 }
