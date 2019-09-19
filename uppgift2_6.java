5e59d5efc1d548da3fc4286c83680bfd42e76e0f
SimonVestman <simonw202@gmail.com>
2019-09-19 11:09:11 +0200

    uppgift2_6

---
+++ uppgift2_6.java
@@ -0,0 +1,20 @@
+/*
+ Skapat 2019-09-19
+*/
+import javax.swing.*;
+import java.util.Scanner;
+
+public class uppgift2_6 { 
+	public static void main(String[] args) {
+		char c1;
+		char c2;
+		
+		c1 = '\u0398';
+		c2 ='\u0398';
+		
+		String s;
+		s = ""+ c1 + "\n" + c2;
+		JOptionPane.showMessageDialog(null,s);
+		
+	}
+}
